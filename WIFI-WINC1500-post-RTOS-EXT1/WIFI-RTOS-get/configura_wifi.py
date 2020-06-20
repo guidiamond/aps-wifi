a8f3e23935ffecd17131e0695105880f828e1b72
guidiamond <guilherme.batista770@gmail.com>
2020-06-20 20:41:09 -0300

    Post method example and python configurable wifi settings

---
+++ WIFI-WINC1500-post-RTOS-EXT1/WIFI-RTOS-get/configura_wifi.py
@@ -0,0 +1,29 @@
+wifi_name = input('Wifi name: ')
+wifi_pass = input('Wifi pass: ')
+server_address = input('External Address:  ')
+
+
+def replace_cfg(path):
+    with open(path, 'r') as main:
+        file = main.readlines()
+
+    for i in range(len(file)):
+        if (file[i].find('#define MAIN_WLAN_SSID') != -1):
+            file[i] = "#define MAIN_WLAN_SSID \"${0}\"\n".format(wifi_name)
+            file[i].replace(file[i], '#define main_wlan_ssid')
+        if (file[i].find('#define MAIN_WLAN_PSK') != -1):
+            file[i] = "#define MAIN_WLAN_PSK \"${0}\"\n".format(wifi_pass)
+        if (file[i].find('#define MAIN_SERVER_NAME') != -1):
+            file[i] = "#define MAIN_SERVER_NAME \"${0}\"\n".format(wifi_pass)
+
+    file = "".join(file)
+    # print(file)
+    with open(path, 'w') as main:
+        main.write(file)
+
+
+replace_cfg("./src/main.h")
+
+# define MAIN_WLAN_SSID                    "OnePlus 6"
+# define MAIN_WLAN_AUTH                    M2M_WIFI_SEC_WPA_PSK
+# define MAIN_WLAN_PSK                     "Guifi345"
---
+++ WIFI-WINC1500-post-RTOS-EXT1/WIFI-RTOS-get/src/defs.h
@@ -0,0 +1,20 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 20/06/2020 17:15:30
+ *  Author: Damn DiamonD
+ */ 
+
+
+#ifndef DEFS_H_
+#define DEFS_H_
+
+
+#define TASK_WIFI_STACK_SIZE      (6*4096/sizeof(portSTACK_TYPE))
+#define TASK_WIFI_PRIORITY        (1)
+#define TASK_PROCESS_STACK_SIZE   (4*4096/sizeof(portSTACK_TYPE))
+#define TASK_PROCESS_PRIORITY     (0)
+
+
+
+#endif 
