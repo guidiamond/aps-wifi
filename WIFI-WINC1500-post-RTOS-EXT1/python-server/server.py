6ab16c493275d34093dafd7e494a0147754e0fc4
guidiamond <guilherme.batista770@gmail.com>
2020-06-24 01:35:03 -0300

    server

--- WIFI-WINC1500-post-RTOS-EXT1/python-server/server.py
+++ WIFI-WINC1500-post-RTOS-EXT1/python-server/server.py
@@ -1,37 +1,27 @@
 from flask import Flask, render_template, request, jsonify
 from flask_restful import Resource, Api
-from datetime import datetime
 
 app = Flask(__name__)
 
-# global led
-# led = 0
 
-@app.route('/')
-def control():
-   return render_template('index.html')
-
-@app.route('/status', methods = ['POST', 'GET'])
+@app.route('/status', methods=['POST', 'GET'])
 def status():
-   led = 0
-   # global led
-   if request.method == 'POST':
-       req = request.get_json()
-       timestamp = req.get('timestamp')
-       id = req.get('id')
-       print("O request vale : ", timestamp, id)
-       print(req)
-       return jsonify({'success':1})
-      # status = request.form
-      # led = status['LED']
-      # return render_template("status.html", status = status)
-   else:
-      return jsonify({'led' : led}), 200
-@app.route('/time', methods = ['GET'])
-def time():
-   if request.method == 'GET':
-      curr_time = datetime.now()
-      return jsonify({'time': curr_time}), 200
+    if request.method == 'POST':
+        req = request.get_json()
+        timestamp = req.get('timestamp')
+        id = req.get('id')
+        print("O request vale : ", timestamp, id)
+        print(req)
+        return jsonify({'sucess': 1}), 200
+    elif request.method == 'GET':
+        return jsonify({'server_status': 1})
+    else:
+        return jsonify({'sucess': 0}), 200
+
+@app.route('/', methods=['GET'])
+def index():
+    if request.method == 'GET':
+        return jsonify({'server_status': 1})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', debug=False)
